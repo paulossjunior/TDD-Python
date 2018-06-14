d38b66cd4c86a6171ed18666c779253b05c6ff4e
Paulo <paulossjunior@gmail.com>
2018-06-14 09:18:18 -0300

    Update calculator.py

--- src/TDD/application/calculator.py
+++ src/TDD/application/calculator.py
@@ -1,14 +1,13 @@
 ##Exemplo retirado do site http://code.tutsplus.com/tutorials/beginning-test-driven-development-in-python--net-30137
+##//lhekheklqhlekhqkehqkehqkhelqw
+##//ljkfhjdhfjkdhfkjlsdhlfkhslkjkljdflksgflsgdf
+##//lkhdsklfskfgshgfsjhgfs
 
-class Calculator______(object):
+class Calculator(object):
     def add(self, x, y):
 
         number_types = (int, float, complex)
-//sasasaslfdjdhfjlhlsjkhflskdjhflkshlfkhsfhskfhsd
-#@323232323233232
-
-lfjdslfsfhjshfjkhskfjhjshfkljshrifjsd
-        if _________isinstance(x, number_types) and _____isinstance(y, number_types):
+        if instance(x, number_types) and instance(y, number_types):
             return x + y
         else:
             raise ValueError
